23937ead706e6ee6927c970a6132dd96add7b0d5
p3nc1l <szocsdavid598@gmail.com>
2025-07-26 12:56:24 +0300

    removed dotenv because it was no longer needed

--- express/index.js
+++ express/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const fatsecret = require("./fatsecret");
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
