5a1b09c5ab7893cf767d3eb6bf1467583d6c780a
p3nc1l <szocsdavid598@gmail.com>
2025-08-10 18:46:27 +0300

    changed letsencrypt folders location to inside the user directory

--- install-certificate.sh
+++ install-certificate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -it --rm -p 80:80 -v "./letsencrypt:/etc/letsencrypt" certbot/certbot certonly --standalone --agree-tos
+docker run -it --rm -p 80:80 -v "$HOME/letsencrypt:/etc/letsencrypt" certbot/certbot certonly --standalone --agree-tos
